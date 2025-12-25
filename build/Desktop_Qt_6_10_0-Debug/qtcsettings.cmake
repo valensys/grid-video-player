db02300e0f3319fa0d978b262ad72de7d0241757
Valentin Wesker <valyok94@gmail.com>
2025-12-25 03:09:07 +0300

    FEAT-1: Add some essential files

--- build/Desktop_Qt_6_10_0-Debug/qtcsettings.cmake
+++ build/Desktop_Qt_6_10_0-Debug/qtcsettings.cmake
@@ -1,12 +1,2 @@
 # This file is managed by Qt Creator, do not edit!
 
-set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/vlv/Qt/Examples/Qt-6.10.0/demos/mediaplayer/build/Desktop_Qt_6_10_0-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "/home/vlv/Qt/6.10.0/gcc_64/bin/qmake" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_COMPILER" "/bin/g++-13" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
-set("QT_MAINTENANCE_TOOL" "/home/vlv/Qt/MaintenanceTool" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_COLOR_DIAGNOSTICS" "ON" CACHE "BOOL" "" FORCE)
-set("CMAKE_C_COMPILER" "/bin/gcc-13" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "/home/vlv/Qt/6.10.0/gcc_64" CACHE "PATH" "" FORCE)
